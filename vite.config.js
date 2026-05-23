c1c638768006a11dca942203ffe0c72aedd87f63
GaelCeballos <gaelceballosnava@gmail.com>
2026-05-23 10:47:34 -0600

    Eliminar archivos .test

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  test: {
+    environment: 'jsdom', // Simula un navegador en la terminal
+    globals: true         // Te permite usar describe, it, expect sin importarlos cada vez
+  }
 })
